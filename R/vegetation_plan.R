4a3f59347f67642eaf17f8cc03ef3cd9637fe26a
audhalbritter <me@audhalbritter.com>
2024-01-04 11:12:04 +0100

    add biomass cleaning code

--- R/vegetation_plan.R
+++ R/vegetation_plan.R
@@ -47,12 +47,38 @@ vegetation_plan <- list(
   tar_target(
     name = ric_depth_clean,
     command = clean_ric_depth(ric_depth_raw)
-  )
+  ),
 
   # tar_target(
   #   name = ric_output,
   #   command = save_csv(file = ric_depth_clean, name = "FUNDER_clean_root_ingrowth_core_depths_2022.csv"),
   #   format = "file"
   # )
+
+  # biomass
+  tar_target(
+    name = biomass_download,
+    command = get_file(node = "tx9r2",
+                       file = "FUNDER Forbs_Graminoids_Bryophytes.xlsx",
+                       path = "raw_data",
+                       remote_path = "1_Vegetation/Raw_data"),
+    format = "file"
+  ),
+
+  tar_target(
+    name = biomass_raw,
+    command = read_excel(biomass_download, sheet = "Dried weight")
+  ),
+
+  tar_target(
+    name = biomass_clean,
+    command = clean_biomass(biomass_raw)
+  ),
+
+  tar_target(
+    name = biomass_output,
+    command = save_csv(file = biomass_clean, name = "FUNDER_clean_biomass_2022.csv"),
+    format = "file"
+  )
 
 )
