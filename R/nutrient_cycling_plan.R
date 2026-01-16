09ba4b438340719ec6c0b0479272166b16023db8
audhalbritter <me@audhalbritter.com>
2026-01-16 10:57:34 +0100

    format all plans

--- R/nutrient_cycling_plan.R
+++ R/nutrient_cycling_plan.R
@@ -1,7 +1,6 @@
 # clean environmental data
 
 nutrient_cycling_plan <- list(
-
   # funder meta data
   tar_target(
     name = funder_meta,
@@ -11,13 +10,14 @@ nutrient_cycling_plan <- list(
   # prs
   tar_target(
     name = prs_download,
-    command = get_file(node = "tx9r2",
-                       file = "FUNDER_raw_PRS_2021.xlsx",
-                       path = "raw_data",
-                       remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"),
+    command = get_file(
+      node = "tx9r2",
+      file = "FUNDER_raw_PRS_2021.xlsx",
+      path = "raw_data",
+      remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"
+    ),
     format = "file"
   ),
-
   tar_target(
     name = prs_raw,
     command = read_excel(prs_download, skip = 4) |>
@@ -30,15 +30,13 @@ nutrient_cycling_plan <- list(
     name = prs_detection_limit,
     command = read_excel(prs_download, skip = 4) |>
       slice(1) |>
-      select(`NO3-N`:Cd)|>
+      select(`NO3-N`:Cd) |>
       pivot_longer(cols = everything(), names_to = "elements", values_to = "detection_limit")
   ),
-
   tar_target(
     name = prs_clean,
     command = clean_prs(prs_raw, prs_detection_limit, funder_meta)
   ),
-
   tar_target(
     name = prs_output,
     command = save_csv(file = prs_clean, name = "FUNDER_clean_available_nutrients_2021.csv"),
@@ -48,23 +46,22 @@ nutrient_cycling_plan <- list(
   # LOI
   tar_target(
     name = loi_download,
-    command = get_file(node = "tx9r2",
-                       file = "FUNDER_raw_LOI_2022.csv",
-                       path = "raw_data",
-                       remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"),
+    command = get_file(
+      node = "tx9r2",
+      file = "FUNDER_raw_LOI_2022.csv",
+      path = "raw_data",
+      remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"
+    ),
     format = "file"
   ),
-
   tar_target(
     name = loi_raw,
     command = read_csv2(loi_download)
   ),
-
   tar_target(
     name = loi_clean,
     command = clean_loi(loi_raw)
   ),
-
   tar_target(
     name = loi_output,
     command = save_csv(file = loi_clean, name = "FUNDER_clean_LOI_2022.csv"),
@@ -74,37 +71,40 @@ nutrient_cycling_plan <- list(
   # CNP
   tar_target(
     name = cnp_depth_download,
-    command = get_file(node = "tx9r2",
-                       file = "FUNDER_raw_CNP_core_depths_2022.csv",
-                       path = "raw_data",
-                       remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"),
+    command = get_file(
+      node = "tx9r2",
+      file = "FUNDER_raw_CNP_core_depths_2022.csv",
+      path = "raw_data",
+      remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"
+    ),
     format = "file"
   ),
-
   tar_target(
     name = cnp_ram_depth_download,
-    command = get_file(node = "tx9r2",
-                       file = "FUNDER_raw_Rambera_CNP_core_depths_2022.csv",
-                       path = "raw_data",
-                       remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"),
+    command = get_file(
+      node = "tx9r2",
+      file = "FUNDER_raw_Rambera_CNP_core_depths_2022.csv",
+      path = "raw_data",
+      remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"
+    ),
     format = "file"
   ),
 
   # sample fresh weight
   tar_target(
     name = cnp_weight_download,
-    command = get_file(node = "tx9r2",
-                       file = "FUNDER_raw_CNP_fresh_sample_weights_2022.xlsx",
-                       path = "raw_data",
-                       remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"),
+    command = get_file(
+      node = "tx9r2",
+      file = "FUNDER_raw_CNP_fresh_sample_weights_2022.xlsx",
+      path = "raw_data",
+      remote_path = "5_Carbon_and_nutrient_cycle/Raw_data"
+    ),
     format = "file"
   ),
-
   tar_target(
     name = cnp_depht_raw,
     command = read_csv2(cnp_depth_download)
   ),
-
   tar_target(
     name = cnp_ram_depht_raw,
     command = read_csv2(cnp_ram_depth_download)
@@ -123,7 +123,4 @@ nutrient_cycling_plan <- list(
     name = cnp_clean,
     command = clean_cnp(cnp_depht_raw, cnp_ram_depht_raw, funder_meta)
   )
-
-
 )
-
