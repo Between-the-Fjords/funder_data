c12dd13948544a263ab5bc6472c3b9ee4e24d030
audhalbritter <me@audhalbritter.com>
2026-01-19 15:30:02 +0100

    remove nematode cleaning. It's done somewhere else

--- R/mesofauna_plan.R
+++ R/mesofauna_plan.R
@@ -24,58 +24,5 @@ mesofauna_plan <- list(
     name = microart_output,
     command = save_csv(file = microart_clean, name = "FUNDER_clean_microarthropod_composition_2022.csv"),
     format = "file"
-  ),
-
-  # nematodes
-  tar_target(
-    name = feeder_download,
-    command = get_file(
-      node = "tx9r2",
-      file = "FUNDER_raw_Nematode_feeding_group_2023.csv",
-      path = "raw_data",
-      remote_path = "2_Micro_and_Mesofauna/Raw_data"
-    ),
-    format = "file"
-  ),
-  tar_target(
-    name = nema_weigth_download,
-    command = get_file(
-      node = "tx9r2",
-      file = "FUNDER_raw_Nematode_sample_weight_2023.csv",
-      path = "raw_data",
-      remote_path = "2_Micro_and_Mesofauna/Raw_data"
-    ),
-    format = "file"
-  ),
-  tar_target(
-    name = nematode_download,
-    command = get_file(
-      node = "tx9r2",
-      file = "FUNDER_raw_Nematodes_families_2023.xlsx",
-      path = "raw_data",
-      remote_path = "2_Micro_and_Mesofauna/Raw_data"
-    ),
-    format = "file"
-  ),
-  tar_target(
-    name = feeder_raw,
-    command = read_csv2(feeder_download)
-  ),
-  tar_target(
-    name = nema_weight_raw,
-    command = read_csv2(nema_weigth_download)
-  ),
-  tar_target(
-    name = nematode_raw,
-    command = read_excel(nematode_download)
-  ),
-  tar_target(
-    name = nematode_clean,
-    command = clean_nematode(nematode_raw, nema_weight_raw, feeder_raw, cnp_depht_raw)
-  ),
-  tar_target(
-    name = nematode_output,
-    command = save_csv(file = nematode_clean, name = "FUNDER_clean_nematode_composition_2022.csv"),
-    format = "file"
   )
 )
