3fde9a0b72a63cf8e683b688a10e6bbf8cb81c26
pgfarsund <pgfarsund@gmail.com>
2026-07-13 20:14:17 +0200

    committing everything and hoping for no conflicts with upstream.

--- _targets.R
+++ _targets.R
@@ -25,7 +25,9 @@ tar_option_set(
     "ncdf4",
     "fs",
     "hms",
-    "fluxible"
+    "fluxible",
+    "osfr",
+    "limma"
   ) # packages that your targets need to run
 )
 #"TNRS"
@@ -41,13 +43,14 @@ tar_source()
 
 #Combine target plans
 combined_plan <- c(
-  vegetation_plan,
-  root_plan,
-  mesofauna_plan,
-  nutrient_cycling_plan,
-  environmenet_plan,
-  flux_plan,
-  data_dic_plan,
-  fungal_community_plan,
-  plant_litter_decomposition_plan
+  # vegetation_plan,
+  # root_plan,
+  # mesofauna_plan,
+  # nutrient_cycling_plan,
+  # environmenet_plan,
+  # flux_plan,
+  # data_dic_plan,
+  # fungal_community_plan,
+  # plant_litter_decomposition_plan,
+  microbial_abundance_plan
 )
