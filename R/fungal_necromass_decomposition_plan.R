a4b89d80a93d2ffff23f4f7303964d0ed9394eab
pgfarsund <pgfarsund@gmail.com>
2026-07-24 14:31:08 +0200

    gave new names to the necromass and litter decomposiiton targets, they are now unique.

--- R/fungal_necromass_decomposition_plan.R
+++ R/fungal_necromass_decomposition_plan.R
@@ -4,7 +4,7 @@ fungal_necromass_decomposition_plan <- list(
 
   # get necromass bags weights pre burial
   tar_target(
-    name = get_pre_burial_weights,
+    name = get_necromass_pre_burial_weights,
     command = get_file(
       node = "tx9r2",
       file = "xxi_FUNDER_raw_fungal_necromass_start_weights.xlsx",
@@ -15,13 +15,13 @@ fungal_necromass_decomposition_plan <- list(
 
   # clean weights pre burial and make tag corrections
   tar_target(
-    name = pre_burial_weights,
-    command = clean_necro_pre(data = get_pre_burial_weights)
+    name = necromass_pre_burial_weights,
+    command = clean_necro_pre(data = get_necromass_pre_burial_weights)
   ),
 
   # get necromass bag weights post burial
   tar_target(
-    name = get_post_burial_weights,
+    name = get_necromass_post_burial_weights,
     command = get_file(
       node = "tx9r2",
       file = "xxi_FUNDER_mass_loss_2022 - fungal_necromass.csv",
@@ -32,23 +32,23 @@ fungal_necromass_decomposition_plan <- list(
 
   # clean weights post burial and make tag corrections
   tar_target(
-    name = post_burial_weights,
-    command = clean_necro_post(data = get_post_burial_weights)
+    name = necromass_post_burial_weights,
+    command = clean_necro_post(data = get_necromass_post_burial_weights)
   ),
 
   # combine pre and post weights and clean
   tar_target(
-    name = post_comments,
-    command = get_necromass_comments(post = post_burial_weights)
+    name = necromass_post_comments,
+    command = get_necromass_comments(post = necromass_post_burial_weights)
   ),
 
   # combine pre weights, post weights, and comments
   tar_target(
     name = clean_fungal_necromass_decomposition,
     command = finish_necromass(
-      pre = pre_burial_weights,
-      post = post_burial_weights,
-      comment = post_comments)
+      pre = necromass_pre_burial_weights,
+      post = necromass_post_burial_weights,
+      comment = necromass_post_comments)
   ),
 
   # save output
@@ -59,14 +59,4 @@ fungal_necromass_decomposition_plan <- list(
       name = "xxi_FUNDER_clean_fungal_necromass_decomposition_2022.csv"
     )
   )
-
 )
-
-
-
-
-# six samples are missing from the dataset:
-# Fau2GB melanised and non-melanised, this plot has not been treated since 2017
-# Ulv1B melanised and non-melanised, this plot is missing in all datasets
-# Ovs1B melanised was not found the field
-# Skj3GB non-melanised was not found in the field
